853becb196b4565fce92fe2b30bb5ca4f4b79c31
Milly Harrison <64021464+HarrNM@users.noreply.github.com>
2020-07-24 15:42:07 +0100

    SAR-5858: Update simpleReactiveMongo

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -15,8 +15,8 @@ object MicroServiceBuild extends Build with MicroService {
     "com.enragedginger" %% "akka-quartz-scheduler" % "1.8.1-akka-2.5.x",
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.8.0",
     "uk.gov.hmrc" %% "domain" % "5.9.0-play-26",
-    "uk.gov.hmrc" %% "mongo-lock" % "6.21.0-play-26",
-    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.26.0-play-26",
+    "uk.gov.hmrc" %% "mongo-lock" % "6.23.0-play-26",
+    "uk.gov.hmrc" %% "simple-reactivemongo" % "7.30.0-play-26",
     "com.typesafe.play" %% "play-json-joda" % "2.6.10",
     "com.typesafe.akka" %% "akka-stream"    % akkaVersion     force(),
     "com.typesafe.akka" %% "akka-protobuf"  % akkaVersion     force(),
@@ -31,6 +31,6 @@ object MicroServiceBuild extends Build with MicroService {
     "com.github.tomakehurst" % "wiremock-jre8" % "2.25.1" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
     "org.mockito" % "mockito-all" % "2.0.2-beta" % scope,
-    "uk.gov.hmrc" %% "reactivemongo-test" % "4.19.0-play-26" % scope
+    "uk.gov.hmrc" %% "reactivemongo-test" % "4.21.0-play-26" % scope
   )
 }
